375fa46a19fcd68d5764e3558bacca47646b5b81
rickyszx <rickydiascosta13@gmail.com>
2021-12-06 16:26:33 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -6,14 +6,34 @@ import Sequelize from 'sequelize'
 const {Op, col} = Sequelize;
 
 
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//GET funciona
+app.get('/nome', async (req, resp) => {
+ 
+        let l = await db.tb_lista_negra.findAll({order: [['id', 'desc']]});
+        resp.send(l);
+
+}) 
+
+
+app.post('/nome', async (req, resp) => {
+        let { nome} = req.body;
+
+        let consulta = await db.tb_lista_negra.findOne({where: {id_nome: nome}});
+
+                let i = await db.tb_lista_negra.create({ 
+                    id_nome: nome, 
+                })
+                resp.send(i)
+
+            }
+)
+//
 
 app.listen(process.env.PORT,
-    x =>  console.log(`Oxe bglh ta lá na ${process.env.PORT}`))
+    x =>  console.log(`Oxi bglh ta lá na ${process.env.PORT}`))
 
 
--- api/src/models/init-models.js
+++ api/src/models/init-models.js
@@ -3,21 +3,18 @@ const DataTypes = _sequelize.DataTypes;
 import _tb_lista_diabolica from  "./tb_lista_diabolica.js";
 import _tb_lista_negra from  "./tb_lista_negra.js";
 import _tb_pessoas from  "./tb_pessoas.js";
-import _tb_produto from  "./tb_produto.js";
 import _tb_rodrigo from  "./tb_rodrigo.js";
 
 export default function initModels(sequelize) {
   const tb_lista_diabolica = _tb_lista_diabolica.init(sequelize, DataTypes);
   const tb_lista_negra = _tb_lista_negra.init(sequelize, DataTypes);
   const tb_pessoas = _tb_pessoas.init(sequelize, DataTypes);
-  const tb_produto = _tb_produto.init(sequelize, DataTypes);
   const tb_rodrigo = _tb_rodrigo.init(sequelize, DataTypes);
 
 return{
     tb_lista_diabolica,
     tb_lista_negra,
     tb_pessoas,
-    tb_produto,
     tb_rodrigo,
   };
 }
