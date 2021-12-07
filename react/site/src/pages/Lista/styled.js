31c28bcbd13db4ce8a00442ef3edb0711a0ea17f
ricky <rickydiascosta13@gmail.com>
2021-12-07 08:54:57 -0300

    ok

--- react/site/src/pages/Lista/styled.js
+++ react/site/src/pages/Lista/styled.js
@@ -3,12 +3,39 @@ import styled from 'styled-components'
 const Container = styled.div`
     
 
-    
+    display: flex;
+    flex-direction: column;
+    max-width: 100%;
+    margin-left: 1em;
+
+    input {
+        width: 209px;
+        height: 30px;
+
+        background: #FFFFFF;
+        border: 1px solid #A8A8A8;
+        box-sizing: border-box;
+        border-radius: 5px;
+        padding-left: .5em;
+
+    }
 
 
+    .inserir{
+        display:flex;
+        flex-direction: row;
+        margin-top: 1em;
+        margin-bottom: 1em
+    }
+
+
+    .eita{
+        margin-left: 0.5em;
+        margin-right: 0.5em;
+    }
 
     thead {
-        background-color: black;
+        background-color: #85d6b3;
     }
 
     table {
@@ -62,13 +89,48 @@ const Container = styled.div`
         cursor: pointer;
     }
 
-   
+    .button-create button:hover {
+        background-color: #aa3997;
+        transition: 2s;
+    }
 
     img {
         cursor: pointer;
     }
 
+    .absolute {
+        color: white;
+        background-color: #119FDC;
+        border: 3px solid white;
+        border-radius: 50%;
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        font-size: .7em;
+    }
 
+    .user-image {
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-start;
+        border-radius: 50%;
+        
+    }
+
+    
+    .user-image img {
+        width: 57px;
+        height: 57px;
+        border-radius: 50%;
+        height: 57px;
+        
+    }
+
+    .imagem-pr > img{
+        max-width:37px;
+        max-height:37px;
+    }
 `
 
 const Conteudo = styled.div`
